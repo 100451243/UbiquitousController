d3ba137af719bc81696d8d691f1c16709976a98f
adelpozoman <adelpozoman@gmail.com>
2023-03-13 11:14:48 +0100

    move html to node

--- node-server/node-QRgenerator.js
+++ node-server/node-QRgenerator.js
@@ -1,41 +1,23 @@
 const http = require('http');
 var QRCode = require('qrcode');
-const express = require('express');
+
+var fs = require('fs');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+let mycode;
 
-QRCode.toString('I am a pony!', { type: 'terminal' }, function (err, url) {
+QRCode.toString('https://tv.adelpozoman.es', { type: 'terminal' }, function (err, url) {
   console.log(url);
   const mycode = url;
 })
 
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  /* QRCode.toString('https://tv.adelpozoman.es', { type: 'terminal' }, function (err, url) {
-    if (err) {
-      res.end('Error generating QR code');
-    } else {
-      res.end(url.toString());
-      //res.end(mycode)
-      //code to show the QR code in the browser
-      //res.end('<img src="' + url + '" />');
-
-    }
-  }); */
-  //load index.html file and show it
-  const express = require('express');
-  const app = express();
-
-  app.get('/', function (request, response) {
-    res.sendFile('index.html');
-  });
-
-
-
+  res.setHeader('Content-Type', 'text/html');
+  html = fs.readFileSync(__dirname + '/index.html', 'utf8');
+  res.end(html);
 });
 
 server.listen(port, hostname, () => {
@@ -43,9 +25,20 @@ server.listen(port, hostname, () => {
 });
 
 
-QRCode.toString('I am a pony!', { type: 'terminal' }, function (err, url) {
-  console.log(url);
-})
 
 
 
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + 'index.html');
+});
+
+const port2 = 3001;
+
+app.listen(port2, () => {
+  console.log(`Server running at http://localhost:${port2}`);
+});
--- node-server/node-webpage.js
+++ node-server/node-webpage.js
@@ -3,9 +3,27 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+var QRCode = require('qrcode');
+QRCode.toString('https://tv.adelpozoman.es', { type: 'terminal' }, function (err, url) {
+  console.log(url);
+  const mycode = url;
+})
+
+
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/html/index.html');
+  res.sendFile(__dirname + 'index.html');
 });
+
+app.get('/node-server/scripts.js', function (req, res) {
+  res.set('Content-Type', 'application/javascript');
+  res.sendFile(__dirname + '/scripts.js');
+});
+
+app.get('/node-server/FFBBDD-6-1.mp4', function (req, res) {
+  res.set('Content-Type', 'video/mp4');
+    res.sendFile(__dirname + '/FFBBDD-6-1.mp4');
+})
 
 const port = 3000;
 
