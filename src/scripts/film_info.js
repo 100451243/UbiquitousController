7ca861df724294dad0d0c9128502de07cba512a7
100451155 <100451155@alumnos.uc3m.es>
2023-04-20 20:41:06 +0200

    para ver pelis chulas

---
+++ src/scripts/film_info.js
@@ -0,0 +1,14 @@
+const title = document.getElementById("name");
+title.innerText = "Titulo";
+
+const logo = document.getElementById("logo");
+logo.src = "../images/foto.jpg";
+
+const player = document.getElementById("player");
+player.poster = "../images/foto.jpg";
+
+const arg = document.getElementById("argument");
+arg.innerText = "Argumento";
+
+const sinopsis = document.getElementById("sinopsis");
+sinopsis.innerText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum";
