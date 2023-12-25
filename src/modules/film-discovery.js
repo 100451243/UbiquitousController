ed60b6c7f3f1f1e9fc0057a9b491da241601fc1d
adelpozoman <adelpozoman@gmail.com>
2023-12-25 22:57:40 +0100

    Sort films based on sort title, if available. fixes #6

--- src/modules/film-discovery.js
+++ src/modules/film-discovery.js
@@ -2,9 +2,30 @@ const fs = require('fs');
 const {readFileSync} = require('fs');
 
 
+// Custom comparator function for sorting
+function compareFilms(film1, film2) {
+    // Check if both films have sorttitle
+    if (film1.metadata && film1.metadata.sorttitle && film2.metadata && film2.metadata.sorttitle) {
+        return film1.metadata.sorttitle.localeCompare(film2.metadata.sorttitle);
+    }
+
+    // If only one of them has sorttitle, compare based on sorttitle and title
+    if (film1.metadata && film1.metadata.sorttitle) {
+        return film1.metadata.sorttitle.localeCompare(film2.title);
+    }
+
+    if (film2.metadata && film2.metadata.sorttitle) {
+        return film1.title.localeCompare(film2.metadata.sorttitle);
+    }
+
+    // If none of them have sorttitle, compare by title
+    return film1.title.localeCompare(film2.title);
+}
+
+
 function createFilmList(given_path) {
-    given_path = require('path').join(fs.readlinkSync(require('path').join(__dirname, '..', "movies"), (err, target) =>
-    {if (!err) console.log(target);}), "/");
+    //given_path = require('path').join(fs.readlinkSync(require('path').join(__dirname, '..', "movies"), (err, target) =>
+    //{if (!err) console.log(target);}), "/");
     console.log("Path: " + given_path)
     let filmList = [];
     //get all folders in the path
@@ -37,6 +58,8 @@ function createFilmList(given_path) {
                     meta.runtime = nfo.match(/<runtime>(.*?)<\/runtime>/)[1];
                     meta.genre = nfo.match(/<genre>(.*?)<\/genre>/)[1];
                     meta.director = nfo.match(/<director>(.*?)<\/director>/)[1];
+                    let match = nfo.match(/<sorttitle>(.*?)<\/sorttitle>/);
+                    meta.sorttitle = match ? match[1] : undefined;
 
                     meta.actors = nfo.match(/<name>(.*?)<\/name>/g).map(actor => {
                         const matches = actor.match(/<name>(.*?)<\/name>/g);
@@ -62,6 +85,7 @@ function createFilmList(given_path) {
             console.log(e)
         }
     }
+    filmList.sort(compareFilms);
     return filmList;
 }
 
